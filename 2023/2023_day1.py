f81fb409202384299ba1819539d472ea9ded3afc
Danny Blom <d.a.m.p.blom@tue.nl>
2023-12-02 13:57:16 +0100

    Add code solutions to day 1 and 2 of Advent of Code 2023

--- 2023/2023_day1.py
+++ 2023/2023_day1.py
@@ -1,20 +1,47 @@
 # Python file for solving the puzzles of day 1 of Advent of Code 2023
+from functools import reduce
 import itertools as it
 import numpy as np
 import sys
 import re
 
-def parse(puzzle_input):
+def parse(filename):
 	'''Method for parsing the puzzle input into a more suitable format for computations'''
-	parsed_input = list(map(str.strip, puzzle_input.readlines()))
+	with open(filename, 'r+') as f:
+		parsed_input = list(map(str.strip, f.readlines()))
 	return parsed_input
 
 def part1(parsed_input):
-	pass
+	result = 0
+	for line in parsed_input:
+		digits = re.findall('\d', line)
+		if len(digits):
+			result += int(digits[0] + digits[-1])
+	return result
 
 def part2(parsed_input):
-	pass
+	mapping = {
+		'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'
+	}
+	pattern = '|'.join(mapping.keys())
+	# print(pattern)
+	
+	result = 0
+	for line in parsed_input:
+		# Look for digit word at start
+		first_digit_pattern = "\d|" + "|".join(mapping.keys())
+		first_digit = re.findall(first_digit_pattern, line)[0]
+		if first_digit in mapping:
+			first_digit = mapping[first_digit]
 
+		final_digit_pattern = "\d|" + "|".join(s[::-1] for s in mapping.keys())
+		reversed_line = line[::-1]
+		final_digit = re.findall(final_digit_pattern, reversed_line)[0]
+		if final_digit[::-1] in mapping:
+			final_digit = mapping[final_digit[::-1]]
+		
+		result += int(first_digit + final_digit)
+	return result
 if __name__ == "__main__":
 	for filename in sys.argv[1:]:
 		print(f'Filename: {filename}')
--- 2023/2023_day2.py
+++ 2023/2023_day2.py
@@ -6,16 +6,42 @@ import re
 
 def parse(puzzle_input):
 	'''Method for parsing the puzzle input into a more suitable format for computations'''
-	parsed_input = list(map(str.strip, puzzle_input.readlines()))
+	with open(puzzle_input, 'r+') as f:
+		parsed_input = list(map(str.strip, f.readlines()))
 	return parsed_input
 
 def part1(parsed_input):
-	pass
+	score = 0
+	for idx, line in enumerate(parsed_input):
+		valid = True
+		# Check red
+		max_red = max(int(num) for num in re.findall('(\d+) red', line))
+		if max_red > UB_RED:
+			continue
+		max_green = max(int(num) for num in re.findall('(\d+) green', line))
+		if max_green > UB_GREEN:
+			continue
+		max_blue = max(int(num) for num in re.findall('(\d+) blue', line))
+		if max_blue > UB_BLUE:
+			continue
+		score += idx + 1
+	return score
 
 def part2(parsed_input):
-	pass
+	score = 0
+	for idx, line in enumerate(parsed_input):
+		max_red = max(int(num) for num in re.findall('(\d+) red', line))
+		max_green = max(int(num) for num in re.findall('(\d+) green', line))
+		max_blue = max(int(num) for num in re.findall('(\d+) blue', line))
+		score += max_red * max_green * max_blue
+	return score
 
 if __name__ == "__main__":
+	global UB_RED
+	global UB_BLUE
+	global UB_GREEN
+
+	UB_RED, UB_GREEN, UB_BLUE = 12, 13, 14
 	for filename in sys.argv[1:]:
 		print(f'Filename: {filename}')
 		parsed_input = parse(filename)
