fe8f347caca2e69ee980cf9376f5879a624ed91b
Danny Blom <d.a.m.p.blom@tue.nl>
2023-12-04 21:23:56 +0100

    Add code for puzzles of day 3 and 4 of Advent of Code 2023

--- 2023/2023_day3.py
+++ 2023/2023_day3.py
@@ -6,19 +6,64 @@ import re
 
 def parse(puzzle_input):
 	'''Method for parsing the puzzle input into a more suitable format for computations'''
-	parsed_input = list(map(str.strip, puzzle_input.readlines()))
+	with open(puzzle_input, 'r+') as f:
+		parsed_input = list(map(str.strip, f.readlines()))
 	return parsed_input
 
 def part1(parsed_input):
-	pass
+	global symbol_positions
+	global start_ints
+	global end_ints
+
+	digits = '1234567890'
+	digits_or_empty = '.' + digits
+
+	symbol_positions = set()
+	start_ints = []
+	end_ints = []
+	
+	for row in range(len(parsed_input)):
+		for col in range(len(parsed_input[row])):
+			if parsed_input[row][col] not in digits_or_empty:
+				symbol_positions.add((row, col))
+				continue
+			if parsed_input[row][col] in digits:
+				if col == 0 or (col >= 1 and parsed_input[row][col-1] not in digits):
+					start_ints.append((row,col))
+				if col == len(parsed_input[row]) - 1 or (col < len(parsed_input[row]) - 1 and parsed_input[row][col+1] not in digits):
+					end_ints.append((row,col)) 
+	score = 0
+	for ((rs,cs), (re,ce)) in zip(start_ints, end_ints):
+		assert rs == re
+		row_range = range(max(rs-1,0), min(rs+2, len(parsed_input)))
+		col_range = range(max(cs-1,0), min(ce+2, len(parsed_input[0])))
+
+		if sum(parsed_input[a][b] not in digits_or_empty for a in row_range for b in col_range) >= 1:
+			score += int(parsed_input[rs][cs:(ce+1)])
+	return score
 
 def part2(parsed_input):
-	pass
+	possible_gears = set(filter(lambda x: parsed_input[x[0]][x[1]] == '*', symbol_positions))
+	score = 0
+	for (rg, cg) in possible_gears:
+		adjacent_numbers = set()
+		for ((rs,cs), (re,ce)) in zip(start_ints, end_ints):
+			columns = list(range(cs, ce+1))
+			if rs in range(rg-1, rg+2) and (cg-1 in columns or cg in columns or cg+1 in columns):
+				adjacent_numbers.add(((rs,cs),(re,ce)))
+		
+		gear_ratio = 1
+		if len(adjacent_numbers) == 2:
+			for ((rs,cs),(re,ce)) in adjacent_numbers:
+				gear_ratio *= int(parsed_input[rs][cs:(ce+1)])
+			score += gear_ratio
+	return score
 
 if __name__ == "__main__":
 	for filename in sys.argv[1:]:
 		print(f'Filename: {filename}')
 		parsed_input = parse(filename)
+		print(parsed_input, len(parsed_input))
 
 		# Solving today's puzzles
 		print(f'Part one: {part1(parsed_input)}')
--- 2023/2023_day4.py
+++ 2023/2023_day4.py
@@ -6,15 +6,40 @@ import re
 
 def parse(puzzle_input):
 	'''Method for parsing the puzzle input into a more suitable format for computations'''
-	parsed_input = list(map(str.strip, puzzle_input.readlines()))
+	with open(puzzle_input, 'r+') as f:
+		parsed_input = []
+		for line in f.readlines():
+			line = line.split(': ')[1]
+			line = line.split('|')
+			parsed_input.append(line)
 	return parsed_input
 
 def part1(parsed_input):
-	pass
+	score = 0
+	for line in parsed_input:
+		winning_numbers = set(map(int, re.findall('\d+', line[0])))
+		own_numbers = set(map(int, re.findall('\d+', line[1])))
+		num_own_winning_numbers = sum(num in winning_numbers for num in own_numbers)
+		if not num_own_winning_numbers == 0:
+			score += 2 ** (num_own_winning_numbers - 1)
+	return score
 
 def part2(parsed_input):
-	pass
+	num_cards = {i: 1 for i in range(len(parsed_input))}
+	maxkey = max(num_cards.keys())
+	score = 0
+	for idx, line in enumerate(parsed_input):
+		winning_numbers = set(map(int, re.findall('\d+', line[0])))
+		own_numbers = set(map(int, re.findall('\d+', line[1])))
+		num_own_winning_numbers = sum(num in winning_numbers for num in own_numbers)
 
+		num_current_card = num_cards[idx]
+		score += num_current_card
+		for l in range(num_own_winning_numbers):
+			if l < maxkey:
+				num_cards[idx + 1 + l] += num_current_card
+	return score		
+			
 if __name__ == "__main__":
 	for filename in sys.argv[1:]:
 		print(f'Filename: {filename}')
