b0ea4132b21acf8ebe39ca15f2fd112fc173f538
Danny Blom <d.a.m.p.blom@tue.nl>
2023-12-13 10:07:32 +0100

    Add solutions to puzzles of Advent of Code 2023 of Days 12 and 13

--- 2023/2023_day12.py
+++ 2023/2023_day12.py
@@ -4,22 +4,91 @@ import numpy as np
 import sys
 import re
 
+def number_of_arrangements(row, seqlens):
+	str_seqlens = ','.join(str(i) for i in seqlens)
+	if (row, str_seqlens) in dp:
+		return dp[row, str_seqlens]
+
+	if not len(row):
+		dp[row, str_seqlens] = len(seqlens) == 0
+		return len(seqlens) == 0
+	
+	if len(seqlens) == 0:
+		dp[row, str_seqlens] = 1 - ('#' in row)
+		return 1 - ('#' in row)
+	
+	if len(row) < seqlens[0]:
+		dp[row, str_seqlens] = 0
+		return 0
+	
+	# If the first character is a dot, we can ignore it
+	first_char = row[0]
+	if first_char == '.':
+		dp[row, str_seqlens] = number_of_arrangements(row[1:], seqlens)
+		return number_of_arrangements(row[1:], seqlens)
+	
+	else:
+		valid_start = True
+		seqlen = seqlens[0]
+		for i in range(seqlen):
+			if row[i] == '.':
+				valid_start = False
+				break
+		if valid_start and len(row) > seqlen and row[seqlen] == '#':
+			valid_start = False
+		
+		if first_char == '#':
+			if valid_start:
+				dp[row, str_seqlens] = number_of_arrangements(row[(seqlen+1):], seqlens[1:])
+				return number_of_arrangements(row[(seqlen+1):], seqlens[1:])
+			else:
+				dp[row, str_seqlens] = 0 
+				return 0
+			
+		else:
+			if valid_start:
+				dp[row, str_seqlens] = number_of_arrangements(row[1:], seqlens) + number_of_arrangements(row[(seqlen+1):], seqlens[1:])	
+				return number_of_arrangements(row[1:], seqlens) + number_of_arrangements(row[(seqlen+1):], seqlens[1:])	
+			else:
+				dp[row, str_seqlens] = number_of_arrangements(row[1:], seqlens)
+				return number_of_arrangements(row[1:], seqlens)
+		
 def parse(puzzle_input):
 	'''Method for parsing the puzzle input into a more suitable format for computations'''
-	parsed_input = list(map(str.strip, puzzle_input.readlines()))
+	with open(puzzle_input, 'r+') as f:
+		parsed_input = []
+		for line in f.readlines():
+			row, sequence_lengths = line.split()
+			sequence_lengths = list(int(i) for i in sequence_lengths.split(','))
+			parsed_input.append((row, sequence_lengths))
 	return parsed_input
 
 def part1(parsed_input):
-	pass
+	score = 0
+	for line in parsed_input:
+		value = number_of_arrangements(*line)
+		score += value
+	return score
 
 def part2(parsed_input):
-	pass
+	score = 0
+	for (row, seqlens) in parsed_input:
+		row = '?'.join(row for _ in range(5))
+		row = re.sub('\.+', '.', row)
+		seqlens = [i for _ in range(5) for i in seqlens]
+		print(row, seqlens)
+		score += number_of_arrangements(row, seqlens)
+	return score
 
 if __name__ == "__main__":
+	global dp
+	dp = {}
+
 	for filename in sys.argv[1:]:
 		print(f'Filename: {filename}')
 		parsed_input = parse(filename)
 
+		
 		# Solving today's puzzles
 		print(f'Part one: {part1(parsed_input)}')
 		print(f'Part two: {part2(parsed_input)}')
--- 2023/2023_day13.py
+++ 2023/2023_day13.py
@@ -4,16 +4,97 @@ import numpy as np
 import sys
 import re
 
+def find_reflection_score(instance):
+	nrows = len(instance)
+	ncols = len(instance[0])
+
+	vertical = True
+	# First look at vertical reflections
+	for i in range(1, ncols):
+		# Vertical line left from i
+		valid = True
+		j = 0
+		while i - 1 - j >= 0 and i + j < ncols:
+			for k in range(nrows):
+				if instance[k][i-1-j] != instance[k][i+j]:
+					valid = False
+					break
+			if not valid:
+				break
+			j += 1
+
+		if valid:
+			return i
+	
+	# Now look at horizontal reflections
+	for i in range(1, nrows):
+		# Horitzontal line above row i
+		valid = True
+		j = 0
+		while i - 1 - j >= 0 and i + j < nrows:
+			for k in range(ncols):
+				if instance[i-1-j][k] != instance[i+j][k]:
+					valid = False
+					break
+			if not valid:
+				break
+			j += 1
+
+		if valid:
+			return 100*i
+
+def find_smudged_reflection_score(instance):
+	nrows = len(instance)
+	ncols = len(instance[0])
+
+	vertical = True
+	# First look at vertical reflections
+	for i in range(1, ncols):
+		# Vertical line left from i
+		nsmudges = 0
+		j = 0
+		while i - 1 - j >= 0 and i + j < ncols:
+			for k in range(nrows):
+				if instance[k][i-1-j] != instance[k][i+j]:
+					nsmudges += 1
+			j += 1
+
+		if nsmudges == 1:
+			return i
+	
+	# Now look at horizontal reflections
+	for i in range(1, nrows):
+		# Horitzontal line above row i
+		nsmudges = 0
+		j = 0
+		while i - 1 - j >= 0 and i + j < nrows:
+			for k in range(ncols):
+				if instance[i-1-j][k] != instance[i+j][k]:
+					nsmudges += 1
+			j += 1
+
+		if nsmudges == 1:
+			return 100*i
+
 def parse(puzzle_input):
 	'''Method for parsing the puzzle input into a more suitable format for computations'''
-	parsed_input = list(map(str.strip, puzzle_input.readlines()))
-	return parsed_input
+	with open(puzzle_input, 'r+') as f:
+		instances = []
+		instance = []
+		for line in f.readlines():
+			if line.strip() != '':
+				instance.append(list(line.strip()))
+			else:
+				instances.append(instance)
+				instance = []
+		instances.append(instance)
+	return instances
 
-def part1(parsed_input):
-	pass
+def part1(instances):
+	return sum(find_reflection_score(instance) for instance in instances)
 
-def part2(parsed_input):
-	pass
+def part2(instances):
+	return sum(find_smudged_reflection_score(instance) for instance in instances)
 
 if __name__ == "__main__":
 	for filename in sys.argv[1:]:
